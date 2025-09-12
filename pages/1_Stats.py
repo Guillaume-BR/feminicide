1f517e3f93dabefb35b57dc3f4449fd493a6314f
Guillaume-BR <114143077+Guillaume-BR@users.noreply.github.com>
2025-09-12 11:10:03 +0200

    Update 1_Stats.py

--- pages/1_Stats.py
+++ pages/1_Stats.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 
 st.title("📊 Statistiques")
+st.write("Est ce que celà dépend des années ?")
 
 #definir working directory
 wd = os.path.dirname(os.path.abspath(__file__))
@@ -13,3 +14,10 @@ df_fem = pd.read_csv(data_path)
 df_fem["année"] = pd.to_datetime(df_fem["date"], errors="coerce").dt.year
 stats = df_fem["année"].value_counts().sort_index()
 st.bar_chart(stats)
+
+st.write("Est ce que celà dépend de l'âge ?")
+age_stats = df_fem["age"].value_counts().sort_index()
+st.bar_chart(age_stats)
+
+st.writre("Et par département ?")
+#faire une carte des départements avec le nombre de féminicides par département
