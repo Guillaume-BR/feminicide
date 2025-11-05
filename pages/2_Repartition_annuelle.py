f3801c0db79eedac6e45d50f507c2e0619e55f1a
Guillaume-BR <114143077+Guillaume-BR@users.noreply.github.com>
2025-11-05 16:37:24 +0100

    Update 2_Repartition_annuelle.py

--- pages/2_Repartition_annuelle.py
+++ pages/2_Repartition_annuelle.py
@@ -1,41 +1,30 @@
 import streamlit as st
 import pandas as pd
 import os
-import matplotlib.pyplot as plt
+import plotly.express as px
 
-# Définir le working directory
+#definir working directory
 wd = os.path.dirname(os.path.abspath(__file__))
 
-# Charger les données
+#charger les données
 fem_path = os.path.join(wd, "..", "data", "processed", "feminicide_2022_2025.csv")
 df_fem = pd.read_csv(fem_path)
 
-# Étude par années
+#Etude par années
 st.title("📊 Répartition annuelle des féminicides")
 
-# Extraire l'année
 df_fem["année"] = pd.to_datetime(df_fem["date"], errors="coerce").dt.year
+nb_fem = df_fem["année"].value_counts().sort_index()
 
-# Compter par année et s’assurer que toutes les années 2022 à 2025 sont présentes
-annees = [2022, 2023, 2024, 2025]
-nb_fem = df_fem["année"].value_counts().reindex(annees, fill_value=0)
+fig_annee = px.bar(
+    x=nb_fem.index,
+    y=nb_fem.values,
+    labels={"x": "Année", "y": "Nombre de féminicides"}
+)
 
-# Créer le barplot
-fig, ax = plt.subplots(figsize=(8, 4))
-ax.bar(nb_fem.index, nb_fem.values, color="#6c8b14")
-ax.set_xlabel("Année")
-ax.set_ylabel("Nombre de féminicides")
-ax.set_title("Répartition annuelle des féminicides (2022–2025)")
+fig_annee.update_xaxes(tickmode="linear", dtick=1)  # ticks entiers (2022, 2023, ...)
 
-# Ajouter les valeurs sur les barres
-for x, y in zip(nb_fem.index, nb_fem.values):
-    ax.text(x, y + 0.5, str(y), ha="center", va="bottom")
-
-ax.set_xticks(annees)
-plt.tight_layout()
-
-# Afficher dans Streamlit
-st.pyplot(fig)
+st.plotly_chart(fig_annee)
 
 st.write("Le nombre de féminicides est malheureusement constant au cours du temps. La baisse observée en 2025 vient seulment du fait que les données s'arrêtent en août 2025. " \
 "Il y a fort à parier que le nombre total de féminicides en 2025 sera similaire aux années précédentes.")
